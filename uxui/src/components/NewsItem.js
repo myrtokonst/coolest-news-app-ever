dd656d74651613bf653ba14a121c048b7d6cfb6f
myrtokonst <myrtokonst@gmail.com>
2019-05-28 18:02:20 +0100

    imagesss

--- uxui/src/components/NewsItem.js
+++ uxui/src/components/NewsItem.js
@@ -2,13 +2,13 @@ import React from 'react';
 import '../NewsItem.css'
 
 const NewsItem = ({ news }) =>
-   <div className="ui card">
-      {console.log(news)}
+   <div className="ui card" >
       <div className="content">
          <div className="header"> {news.title}</div>
          <div className="meta">
             <span>{news.published}</span>
          </div>
+         <img src={news.thread.main_image} />
          <p> {news.text.slice(0, 300)}</p>
       </div>
    </div>
