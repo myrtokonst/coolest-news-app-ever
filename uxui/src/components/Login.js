402acebc3a397c40f88955baf0d335c29fe962bb
oli-gra <oliver.grant@gmail.com>
2019-05-28 17:43:18 +0100

    okay

--- uxui/src/components/Login.js
+++ uxui/src/components/Login.js
@@ -1,12 +1,66 @@
 import React, { PureComponent } from 'react';
 
 class Login extends PureComponent {
-   state = {}
+
+   state = {
+      user: {
+         username: '',
+         last_login: ''
+      }
+   }
+
+   handleInputChange = (event) => {
+      this.setState({
+         user: {
+            ...this.state.user,
+            username: event.target.value
+         }
+      })
+   }
+
+   handleSubmit = (event) => {
+      event.preventDefault()
+      const time = Date.now()
+      this.setState({
+         user: {
+            ...this.state.user,
+            last_login: time
+         }
+      })
+      const username = this.state.user.username
+      // debugger
+      return fetch('http://localhost:3000/users', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            user: {
+               username: username,
+               last_login: time
+            }
+         })
+      }
+      ).then(resp => resp.json()).then(data => this.props.updateNews(data))
+   }
    render() {
+      let { user } = this.state
+      let { username, last_login } = user
       return (
-         <div>
-            Login
-         </div>
+         <form onSubmit={this.handleSubmit}>
+            <div>
+               <label>
+                  Username
+             <input id="username" name="username" type="text"
+                     value={username}
+                     onChange={event => this.handleInputChange(event)}
+                  />
+               </label>
+            </div>
+            <div>
+               <button type="submit">Log in</button>
+            </div>
+         </form>
       )
    }
 }
